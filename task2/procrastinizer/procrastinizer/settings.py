cac41994874c464914ac78a720964cc27119e0de
wiki-fan <uberslowpoke@gmail.com>
2018-02-25 00:21:44 +0300

    Docker done

--- task2/procrastinizer/procrastinizer/settings.py
+++ task2/procrastinizer/procrastinizer/settings.py
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'procrastinizer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'guest',
+        'PASSWORD': 'guest',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
--- task2/procrastinizer/todolist/views.py
+++ task2/procrastinizer/todolist/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from .models import Task
 from .forms import TaskCreationForm
 
 
 def index(request):
-    latest_tasks_list = Task.objects.order_by('-set_date')
+    latest_tasks_list = Task.objects.order_by('id')
     context_dict = {'active_tasks_list': latest_tasks_list.filter(is_done=False),
                     'completed_tasks_list': latest_tasks_list.filter(is_done=True)}
     if request.method == 'POST':
         form = TaskCreationForm(request.POST, request.FILES)
         if form.is_valid():
-            # Filling some fields directly from form
             instance = form.save(commit=False)
             instance.is_done = False
             instance.save()
@@ -26,14 +25,16 @@ def index(request):
 
 
 def task_info(request, task_id):
-    task = Task.objects.get(id=task_id)
+    try:
+        task = Task.objects.get(id=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Poll does not exist")
     context_dict = {'task': task}
 
     if request.method == 'POST':
         task.is_done = True
         task.save()
 
-    #context_dict['task_creation_form'] = form
     return render(request, 'todolist/task_info.html', context_dict)
 
 def do_task(request, task_id):
